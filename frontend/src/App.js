4d149b07561072770c984f26614d0548f87ee36e
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-26 09:58:42 -0300

    Updated files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from "./components/Header";
+import Blogs from "./components/Blogs";
+import JobMarket from "./components/JobMarket";
+import Salaries from "./components/Salaries";
+import UpcomingEvents from "./components/UpcomingEvents";
+import UniversitiesInfo from "./components/UniversitiesInfo";
+import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <div className="app">
+
+                <div className="bg-blue-500 text-white p-4">
+                    Hello, Tailwind CSS!
+                </div>
+
+                <Header />
+
+                <Routes>
+                    <Route path="/blogs" element={<Blogs />} />
+                    <Route path="/job-market" element={<JobMarket />} />
+                    <Route path="/salaries" element={<Salaries />} />
+                    <Route path="/upcoming-events" element={<UpcomingEvents />} />
+                    <Route path="/universities-info" element={<UniversitiesInfo />} />
+                </Routes>
+
+                <Footer />
+            </div>
+        </Router>
+    );
+};
+
+const navigateTo = (path) => {
+    window.location.href = `/${path}.html`;
+};
 
 export default App;
