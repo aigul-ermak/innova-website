a6ca41f299f0c3f85a52e95ce4b2065fb579246e
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-26 12:43:22 -0300

    Updated dashboard: changes styles

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -3,20 +3,40 @@ import {Link} from "react-router-dom";
 
 const Dashboard = () => {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-            <Link to='/selfassessment' exact>
-                <button className='btn btn-primary'>
-                    <span>Self Assessment</span>
-                </button>
-            </Link>
+        <div className="text-center py-12 px-4">
+            <h2 className="text-2xl font-bold">Get In Touch</h2>
+            <p className="mt-4 text-gray-700 max-w-2xl mx-auto">We are here to help you. Reach out to us via any of the
+                following methods.</p>
 
-            <Link to='/quiz' exact>
-                <button className='btn btn-primary'>
-                    <span>Career Quiz</span>
-                </button>
-            </Link>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8 animate-fadeIn">
+                <div className="p-4 shadow-lg rounded-lg bg-green-100 hover:bg-green-200 transition-colors">
+                    <Link to='/selfassessment' exact>
+                        <button
+                            className='text-xl font-bold'>
+                            <span>Self Assessment</span>
+                        </button>
+                    </Link>
+                </div>
+
+                <div className="p-4 shadow-lg rounded-lg bg-green-100 hover:bg-green-200 transition-colors">
+                    <Link to='/quiz' exact>
+                        <button
+                            className='text-xl font-bold'>
+                            <span>Career Quiz</span>
+                        </button>
+                    </Link>
+                </div>
+
+                <div className="p-4 shadow-lg rounded-lg bg-green-100 hover:bg-green-200 transition-colors">
+                    <Link to='#' exact>
+                        <button
+                            className='text-xl font-bold'>
+                            <span>Chat with AI</span>
+                        </button>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
-
 export default Dashboard;
