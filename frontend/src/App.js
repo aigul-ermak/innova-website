1e593f711a4b90753099c68f319d54bed28f5732
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-26 12:19:08 -0300

    Updated app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from "./components/Header";
 import Blogs from "./components/Blogs";
 import JobMarket from "./components/JobMarket";
@@ -7,28 +7,36 @@ import Salaries from "./components/Salaries";
 import UpcomingEvents from "./components/UpcomingEvents";
 import UniversitiesInfo from "./components/UniversitiesInfo";
 import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Dashboard from "./components/Dashboard";
+import SelfAssessment from "./components/SelfAssesment";
+import Quiz from "./components/Quiz";
 
 const App = () => {
     return (
         <Router>
-            <div className="app">
+            <div className="flex flex-col min-h-screen">
 
-                <div className="bg-blue-500 text-white p-4">
-                    Hello, Tailwind CSS!
+                <Header/>
+
+                <div className="flex-grow">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/blogs" element={<Blogs />} />
+                        <Route path="/job-market" element={<JobMarket />} />
+                        <Route path="/salaries" element={<Salaries />} />
+                        <Route path="/upcoming-events" element={<UpcomingEvents />} />
+                        <Route path="/universities-info" element={<UniversitiesInfo />} />
+                        <Route path="/dashboard" element={<Dashboard />} />
+                        <Route path="/selfassessment" element={<SelfAssessment />} />
+                        <Route path="/quiz" element={<Quiz />} />
+                    </Routes>
                 </div>
 
-                <Header />
+                <Footer/>
 
-                <Routes>
-                    <Route path="/blogs" element={<Blogs />} />
-                    <Route path="/job-market" element={<JobMarket />} />
-                    <Route path="/salaries" element={<Salaries />} />
-                    <Route path="/upcoming-events" element={<UpcomingEvents />} />
-                    <Route path="/universities-info" element={<UniversitiesInfo />} />
-                </Routes>
-
-                <Footer />
             </div>
+
         </Router>
     );
 };
