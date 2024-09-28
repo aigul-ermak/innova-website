eb0a6eea44f34f6fbf4baf2e05b2968a8443ccf3
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-28 14:36:34 -0300

    Removed comments

--- frontend/src/components/SelfAssesment.js
+++ frontend/src/components/SelfAssesment.js
@@ -70,10 +70,7 @@ const SelfAssessment = () => {
     const currentQuestion = questions[currentQuestionIndex];
 
     return (
-        // <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-        //     <h1 className="text-3xl font-bold">Self Assessment</h1>
-        //     <p className="mt-4 text-lg">This is the Self Assessment page.</p>
-        // </div>
+
 
         <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
             <div className="question text-xl font-bold mb-4">{currentQuestion.question}</div>
