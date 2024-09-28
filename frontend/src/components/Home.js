de6ce01c649dcb116507f0875393291dabd1f374
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2024-09-28 13:57:04 -0300

    Updated Home component: removed link to Dashboard

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -21,14 +21,6 @@ const Home = () => {
                         sunt amet fugiat
                         veniam occaecat fugiat aliqua.
                     </p>
-                    <div className="mt-10 flex items-center justify-center gap-x-6">
-                        <Link
-                            to="/dashboard"
-                            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
-                            Get started
-                        </Link>
-                    </div>
                 </div>
             </div>
             <div
